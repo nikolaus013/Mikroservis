154f54d696cf7af3f574c92b44ef69c22f8a9340
ndjordjevic16 <ndjordjevic16@raf.rs]>
2019-06-11 17:18:46 +0200

    Initial commit

---
+++ Filmovi/src/main/java/com/filmovi/Filmovi/FilmoviApplication.java
@@ -0,0 +1,13 @@
+package com.filmovi.Filmovi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilmoviApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilmoviApplication.class, args);
+	}
+
+}
---
+++ Filmovi/src/test/java/com/filmovi/Filmovi/FilmoviApplicationTests.java
@@ -0,0 +1,16 @@
+package com.filmovi.Filmovi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FilmoviApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
